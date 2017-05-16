b7e3e78ca88e3d971f0fa17d0f1b25ce49322c83
Paulo Roberto <prpoliveira@hotmail.com>
2017-05-16 10:15:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 
+error_reporting(E_ALL);
+ini_set("display_errors","On");
+
 spl_autoload_register(function ($class){
     if(strpos($class, 'Controller') > -1) {
         if(file_exists('controllers/'.$class.'.php')) {
